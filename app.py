79236de3072afa0def0e178c0e4318eb8bf46caa
Huzaifa Kamran <huzaifa.2014@hotmail.com>
2023-08-19 18:36:52 +0500

    second commit

--- app.py
+++ app.py
@@ -9,3 +9,6 @@ def main():
 @app.route('/<int:id>', methods=['GET'])
 def home(id):
     return render_template('index.html',id=id)
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5000,host='0.0.0.0')
